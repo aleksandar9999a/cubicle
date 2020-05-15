6b6a57a9be481387cd4439e002113adbec303910
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-15 14:40:32 +0300

    Added create, about, 404 handlers

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,9 @@
 const mainCotroller = require('./../controllers/main.controller');
 
 module.exports = (app) => {
+    app.use('/404', mainCotroller.notFound);
+    app.use('/about', mainCotroller.about);
+    app.use('/create', mainCotroller.create);
     app.use('/details/:id', mainCotroller.details);
     app.use('/', mainCotroller.index);
 };
--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -10,11 +10,30 @@ function index(req, res, next) {
 function details(req, res, next) {
     const id = Number(req.params.id);
     cubeModel.getOne(id).then(cube => {
+        if (!cube) {
+            res.redirect('/404');
+            return;
+        }
         res.render(path.resolve('./views/details.hbs'), { cube });
     }).catch(next);
 }
 
+function notFound(req, res, next) {
+    res.render(path.resolve('./views/404.hbs'));
+}
+
+function create(req, res, next) {
+    res.render(path.resolve('./views/create.hbs'));
+}
+
+function about(req, res, next) {
+    res.render(path.resolve('./views/about.hbs'));
+}
+
 module.exports = {
     index,
-    details
+    details,
+    notFound,
+    about,
+    create
 };
