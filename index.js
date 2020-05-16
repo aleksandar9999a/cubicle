febec54071851b3d532280e9cb5544e006ddedaa
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-16 14:40:59 +0300

    Added mongodb, mongoose

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const env = process.env.NODE_ENV || 'development';
 global.__basedir = __dirname;
-const config = require('./config/config')[env];
-const app = require('express')();
+const dbConnector = require('./config/db');
 
-require('./config/express')(app);
-require('./config/routes')(app);
+dbConnector().then(() => {
+    const config = require('./config/config');
+    const app = require('express')();
 
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+    require('./config/express')(app);
+    require('./config/routes')(app);
+
+    app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+}).catch(console.error);
+
