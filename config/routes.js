fc1d3827887080939c5dfdd61ac6f2b5bdd4e5cc
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-17 11:07:08 +0300

    Fix url adress for add accessory

--- config/routes.js
+++ config/routes.js
@@ -2,12 +2,12 @@ const mainCotroller = require('./../controllers/main.controller');
 
 module.exports = (app) => {
     app.use('/details/:id', mainCotroller.details);
-    app.use('/about', mainCotroller.about);
-    app.use('/404', mainCotroller.notFound);
+    app.post('/create/accessory', mainCotroller.postAccessories);
+    app.use('/create/accessory', mainCotroller.getAccessories);
     app.post('/create', mainCotroller.postCreate);
     app.use('/create', mainCotroller.getCreate);
-    app.post('/addAccessory', mainCotroller.postAccessories);
-    app.use('/addAccessory', mainCotroller.getAccessories);
+    app.use('/about', mainCotroller.about);
+    app.use('/404', mainCotroller.notFound);
     app.post('/', mainCotroller.postIndex);
     app.use('/', mainCotroller.getIndex);
 };
