5d93f8c161bc196e60ac0b5080c7607b1d931083
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-17 11:00:49 +0300

    Added more validations

--- models/cube.js
+++ models/cube.js
@@ -1,10 +1,29 @@
 const mongoose = require('mongoose');
 
-const modelSchema = new mongoose.Schema({
-    name: String,
-    desc: String,
-    image: String,
-    difficulty: Number
+const cubeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    desc: {
+        type: String,
+        maxlength: 300
+    },
+    image: {
+        type: String
+    },
+    difficulty: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 6
+    },
+    accessories: [
+        { 
+            type: mongoose.Types.ObjectId ,
+            ref: 'Accessories'
+        }
+    ]
 });
 
-module.exports = mongoose.model('Cubes', modelSchema);
+module.exports = mongoose.model('Cubes', cubeSchema);
