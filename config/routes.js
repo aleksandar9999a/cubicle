9d85537008085f09c7c3d8bc59097dcc58681a75
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-17 14:24:09 +0300

    Added routes for login and register

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,18 @@
 const mainCotroller = require('./../controllers/main.controller');
+const authCotroller = require('./../controllers/auth');
 
 module.exports = (app) => {
     app.post('/create/accessory', mainCotroller.postAccessories);
-    app.use('/create/accessory', mainCotroller.getAccessories);
+    app.get('/create/accessory', mainCotroller.getAccessories);
     app.post('/attach/accessory/:id', mainCotroller.postAttachAccessory);
     app.get('/attach/accessory/:id', mainCotroller.getAttachAccessory);
-    app.use('/details/:id', mainCotroller.details);
+    app.get('/details/:id', mainCotroller.details);
     app.post('/create', mainCotroller.postCreate);
-    app.use('/create', mainCotroller.getCreate);
-    app.use('/about', mainCotroller.about);
+    app.get('/create', mainCotroller.getCreate);
+    app.get('/about', mainCotroller.about);
+    app.get('/login', authCotroller.getLogin);
+    app.get('/register', authCotroller.getRegister);
     app.use('/404', mainCotroller.notFound);
     app.post('/', mainCotroller.postIndex);
-    app.use('/', mainCotroller.getIndex);
+    app.get('/', mainCotroller.getIndex);
 };
