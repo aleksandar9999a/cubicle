34a183ba0f79fc93fea651b34d439b411ea52386
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-17 14:41:39 +0300

    Added full postRegister

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { userModel } = require('../models');
 
 function getLogin(req, res, next) {
     res.render(path.resolve('./views/loginPage.hbs'));
@@ -9,11 +10,34 @@ function getRegister(req, res, next) {
 }
 
 function postLogin(req, res, next) {
-    
+
 }
 
 function postRegister(req, res, next) {
-    
+    const { username, password, repeatPassword } = req.body;
+    if (password !== repeatPassword) {
+        res.render(
+            path.resolve('./views/registerPage.hbs'),
+            { errors: { repeatPassword: 'Two password are not match!' } }
+        );
+        return;
+    }
+
+    userModel.findOne({ username }).then(user => {
+        if (user) {
+            res.render(
+                path.resolve('./views/registerPage.hbs'),
+                { errors: { username: 'Username already exist!' } }
+            );
+            return;
+        }
+
+        return userModel.create({ username, password })
+    })
+    .then(() => {
+        res.redirect('/login');
+    })
+    .catch(next);
 }
 
 
