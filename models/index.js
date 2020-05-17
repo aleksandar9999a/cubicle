3f31e1d25b3984605f9bd8d155cdaf1267296191
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-17 14:40:24 +0300

    Imports all models in one file and export

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const cubeModel = require('./cube');
+const userModel = require('./user');
+const accessoriesModel = require('./accessories');
+const blacklistModel = require('./token-blacklist');
+
+module.exports = {
+    cubeModel,
+    userModel,
+    accessoriesModel,
+    blacklistModel
+}
