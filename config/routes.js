1a97e8a61a2b1c0ab1771c8a2274f3a9f196c542
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-17 11:37:19 +0300

    Added more routes

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,11 @@
 const mainCotroller = require('./../controllers/main.controller');
 
 module.exports = (app) => {
-    app.use('/details/:id', mainCotroller.details);
     app.post('/create/accessory', mainCotroller.postAccessories);
     app.use('/create/accessory', mainCotroller.getAccessories);
+    app.post('/attach/accessory/:id', mainCotroller.postAttachAccessory);
+    app.get('/attach/accessory/:id', mainCotroller.getAttachAccessory);
+    app.use('/details/:id', mainCotroller.details);
     app.post('/create', mainCotroller.postCreate);
     app.use('/create', mainCotroller.getCreate);
     app.use('/about', mainCotroller.about);
