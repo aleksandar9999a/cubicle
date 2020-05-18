2ad9a8fa5c29f10f92b21a95b1b994157d2976e0
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-18 11:15:00 +0300

    Added auth middleware

---
+++ utils/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('./jwt');
+const appConfig = require('./../app-config');
+const { userModel, blacklistModel } = require('./../models');
+
+
+function auth(req, res, next) {
+    const token = req.cookies[appConfig.authCookie] || '';
+    Promise.all([jwt.verifyToken(token), blacklistModel.findOne({ token })])
+        .then(([data, blacklistToken]) => {
+            if (blacklistToken) {
+                return Promise.reject(new Error('Blacklisted token!'))
+            }
+            userModel.findById(data.id)
+                .then(user => {
+                    req.user = user;
+                    next()
+                })
+        }).catch(err => {
+            if (['token expired', 'Blacklisted token!', 'jwt must be provided'].includes(err.message)) {
+                res.redirect('/login');
+                return;
+            }
+            next(err);
+        })
+
+}
+
+module.exports = auth;
