01cf300e9daa1ce71787bec441039cd6129dcc87
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-18 11:15:16 +0300

    Fix verifyToken to return promise

--- utils/jwt.js
+++ utils/jwt.js
@@ -2,11 +2,19 @@ const jwt = require('jsonwebtoken');
 const secret = 'shhhhh';
 
 function createToken(data) {
-    return jwt.sign(data, secret);
+    return jwt.sign(data, secret, { expiresIn: '10m' });
 }
 
 function verifyToken(token) {
-    return jwt.verify(token, secret);
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, secret, (err, data) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(data);
+        })
+    })
 }
 
 module.exports = {
