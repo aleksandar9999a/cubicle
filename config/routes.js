1645859e3dd9415001c53b2a22aa28725652f972
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-18 11:16:09 +0300

    Added auth middleware, logout

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,6 @@
 const mainCotroller = require('./../controllers/main.controller');
 const authCotroller = require('./../controllers/auth');
+const { auth } = require('./../utils');
 
 module.exports = (app) => {
     app.post('/create/accessory', mainCotroller.postAccessories);
@@ -7,13 +8,14 @@ module.exports = (app) => {
     app.post('/attach/accessory/:id', mainCotroller.postAttachAccessory);
     app.get('/attach/accessory/:id', mainCotroller.getAttachAccessory);
     app.get('/details/:id', mainCotroller.details);
-    app.post('/create', mainCotroller.postCreate);
-    app.get('/create', mainCotroller.getCreate);
+    app.post('/create', auth, mainCotroller.postCreate);
+    app.get('/create', auth, mainCotroller.getCreate);
     app.get('/about', mainCotroller.about);
     app.post('/login', authCotroller.postLogin);
     app.get('/login', authCotroller.getLogin);
     app.post('/register', authCotroller.postRegister);
     app.get('/register', authCotroller.getRegister);
+    app.get('/logout', authCotroller.logout);
     app.use('/404', mainCotroller.notFound);
     app.post('/', mainCotroller.postIndex);
     app.get('/', mainCotroller.getIndex);
